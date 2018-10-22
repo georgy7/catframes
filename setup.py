cfc430537196b87dbdd377375b4d4c1f104853da
Георгий У <httpcatharsis@gmail.com>
2018-10-23 00:10:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import setuptools
 
 setuptools.setup(
+  classifiers=[
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
   python_requires=">=3.3"
 )
