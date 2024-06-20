0bda2458eced300d1b811ab88ea32adf4ae0b38a
neiroslav <bogdansir1807@gmail.com>
2024-06-20 04:48:39 +0300

    The "Styles" class has been removed. Styles will be assembled with other view components (such as scale, sizes, etc.) in WindowMixin tools.
    The new scale system allows for scaling the window's size according to the screen resolution.
    
    The Widget.place method has been replaced with Frame.pack and Widget.grid.

--- src/catframes/catmanager.py
+++ src/catframes/catmanager.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from tkinter import Tk, Toplevel, ttk, font
+from tkinter import Tk, Toplevel, ttk
 from abc import ABC, abstractmethod
+
+
+# временные глобальные переменные
+MAJOR_SCALING: float = 1.0
+TTK_THEME: str | None = None
 
 
 class Lang:
@@ -23,7 +28,7 @@ class Lang:
             'root.openSets': 'Settings',
             'root.newTask': 'New task',
 
-            'sets.title': 'Settings Menu',
+            'sets.title': 'Settings',
             'sets.lbLang': 'Language:',
             'sets.btApply': 'Apply',
             'sets.btSave': 'Save',
@@ -36,7 +41,7 @@ class Lang:
             'root.openSets': 'Настройки',
             'root.newTask': 'Новая задача',
 
-            'sets.title': 'Меню Настроек',
+            'sets.title': 'Настройки',
             'sets.lbLang': 'Язык:',
             'sets.btApply': 'Применить',
             'sets.btSave': 'Сохранить',
@@ -68,16 +73,6 @@ class Lang:
             return '-----'
 
 
-class Styles(ttk.Style):
-    """Класс для определения стилей виджетов"""
-
-    def __init__(self, root: Tk):
-        super().__init__()
-        self.theme_use('alt')  # одна из стандартных тем, для примера
-        self.configure(style='TButton', font='14')  # шрифт текста в кнопке
-        root.option_add("*Font", font.Font(size=14))  # шрифты остальных виджетов
-
-
 #
 
 
@@ -91,14 +86,16 @@ class WindowMixin(ABC):
     protocol: Tk.protocol   # появятся автоматически при
     destroy: Tk.destroy     # наследовании от Tk или Toplevel
 
+    size: tuple[int, int]   # размеры (ширина, высота) окна
     name: str               # имя окна для словаря всех окон
-    widgets: dict           # словарь виджетов окна
+    widgets: dict[str, ttk.Widget]  # словарь виджетов окна
 
     # настройка окна, вызывается через super в конце __init__ окна
     def _default_set_up(self):
         self.all_windows[self.name] = self  # регистрация окна в словаре
         self.protocol("WM_DELETE_WINDOW", self.close)  # что выполнять при закрытии
 
+        self._set_style()     # настройка внешнего вида окна
         self._init_widgets()  # создание виджетов
         self.update_texts()   # установка текста нужного языка
         self._pack_widgets()  # расстановка виджетов
@@ -114,8 +111,24 @@ class WindowMixin(ABC):
     # обновление текстов всех виджетов окна, исходя из языка
     def update_texts(self) -> None:
         self.title(Lang.read(f'{self.name}.title'))
+
         for w_name, widget in self.widgets.items():
             widget.config(text=Lang.read(f'{self.name}.{w_name}'))
+
+    # настройка стиля окна, исходя из разрешения экрана
+    def _set_style(self) -> None:
+
+        screen_height = self.winfo_screenheight()  # достаём высоту экрана
+        scale = (screen_height/540)                # индекс масштабирования
+        scale *= MAJOR_SCALING                     # домножаем на глобальную
+
+        style=ttk.Style()
+        if TTK_THEME: style.theme_use(TTK_THEME)   # применение темы, если есть
+        style.tk.call('tk', 'scaling', scale)      # установка масштаба окна
+
+        x, y = self.size                   # забираем объявленные размеры окна
+        x, y = int(x*scale), int(y*scale)  # масштабируем их
+        self.geometry(f'{x}x{y}')          # и присваиваем их окну
 
     # метод для создания и настройки виджетов
     @abstractmethod
@@ -137,12 +150,13 @@ class RootWindow(Tk, WindowMixin):
     def __init__(self):
         super().__init__()
         self.name = 'root'
-        self.widgets = {}
+        
+        self.widgets: dict[str, ttk.Widget] = {}
+        self.frames:  dict[str, ttk.Frame] = {}
 
-        self.geometry("600x500")  # размеры  окна
+        self.size = 300, 250
         self.resizable(False, False)  # нельзя растягивать
 
-        self.styles = Styles(self)
         super()._default_set_up()
 
     # при закрытии окна
@@ -155,24 +169,32 @@ class RootWindow(Tk, WindowMixin):
 
         # открытие окна с новой задачей
         def open_new_task():
-            if self.all_windows.get('task'):  # если нашёл окно в словаре регистрации
-                return self.all_windows['task'].focus()  # фокусируется на нём
-            self.all_windows['task'] = NewTaskWindow(root=self)  # если не нашёл - создать
+            if not self.all_windows.get('task'):  # если не нашёл окно в словаре
+                NewTaskWindow(root=self)  # создать окно (само добавится в словарь)
+            self.all_windows['task'].focus()  # фокусируется на нём
 
         # открытие окна настроек
         def open_settings():
-            if self.all_windows.get('sets'):
-                return self.all_windows['sets'].focus()
-            self.all_windows['sets'] = SettingsWindow(root=self)
+            if not self.all_windows.get('sets'):
+                SettingsWindow(root=self)
+            self.all_windows['sets'].focus()
+
+        # создание фреймов
+        self.frames['upperBar'] = upperBar = ttk.Frame(self)
 
         # создание виджетов, привязывание функций
-        self.widgets['newTask'] = ttk.Button(self, command=open_new_task)
-        self.widgets['openSets'] = ttk.Button(self, command=open_settings)
+        self.widgets['newTask'] = ttk.Button(upperBar, command=open_new_task)
+        self.widgets['openSets'] = ttk.Button(upperBar, command=open_settings)
 
     # расположение виджетов
     def _pack_widgets(self):
-        self.widgets['newTask'].place(x=15, y=15, width=150, height=30)
-        self.widgets['openSets'].place(relx=1.0, x=-165, y=15, width=150, height=30)
+        self.frames['upperBar'].pack(fill='x', padx=15, pady=15)
+
+        self.widgets['newTask'].pack(side='left')
+        self.widgets['openSets'].pack(side='right')
+
+
+#
 
 
 class SettingsWindow(Toplevel, WindowMixin):
@@ -181,9 +203,10 @@ class SettingsWindow(Toplevel, WindowMixin):
     def __init__(self, root: RootWindow):
         super().__init__(master=root)
         self.name = 'sets'
-        self.widgets = {}
 
-        self.geometry("260x200")
+        self.widgets: dict[str, ttk.Widget] = {}
+
+        self.size = 130, 100
         self.resizable(False, False)
 
         super()._default_set_up()
@@ -205,22 +228,34 @@ class SettingsWindow(Toplevel, WindowMixin):
             self.close()
 
         # создание виджетов, привязывание функций
-        self.widgets['btApply'] = ttk.Button(self, command=apply_settings)
-        self.widgets['btSave'] = ttk.Button(self, command=save_settings)
         self.widgets['lbLang'] = ttk.Label(self)
         self.widgets['cmbLang'] = ttk.Combobox(  # виджет выпадающего списка
             self,
             values=Lang.get_all(),  # вытягивает список языков
-            state='readonly'  # запрещает вписывать, только выбирать
+            state='readonly',  # запрещает вписывать, только выбирать
+            width=7
         )
+        
+        self.widgets['btApply'] = ttk.Button(self, command=apply_settings, width=7)
+        self.widgets['btSave'] = ttk.Button(self, command=save_settings, width=7)
 
     # расположение виджетов
     def _pack_widgets(self):
-        self.widgets['lbLang'].place(x=15, y=15, width=110, height=30)
-        self.widgets['cmbLang'].place(relx=1.0, x=-125, y=15, width=110, height=30)
+
+        for c in range(2): 
+            self.columnconfigure(index=c, weight=1)
+        for r in range(5): 
+            self.rowconfigure(index=r, weight=1)
+        
+        self.widgets['lbLang'].grid(row=0, column=0, sticky='w', padx=20)
+        self.widgets['cmbLang'].grid(row=0, column=1, sticky='ew', padx=(5 ,15))
         self.widgets['cmbLang'].current(newindex=Lang.current_index)  # подставляем в ячейку текущий язык
-        self.widgets['btApply'].place(rely=1.0, x=15, y=-45, width=110, height=30)
-        self.widgets['btSave'].place(rely=1.0, relx=1.0, x=-125, y=-45, width=110, height=30)
+
+        self.widgets['btApply'].grid(row=4, column=0, sticky='ew', padx=(15, 5), ipadx=30)
+        self.widgets['btSave'].grid(row=4, column=1, sticky='ew', padx=(5, 15), ipadx=30)
+
+
+#
 
 
 class NewTaskWindow(Toplevel, WindowMixin):
@@ -231,7 +266,7 @@ class NewTaskWindow(Toplevel, WindowMixin):
         self.name = 'task'
         self.widgets = {}
 
-        self.geometry("400x300")
+        self.size = 200, 150
         self.resizable(False, False)
 
         super()._default_set_up()
