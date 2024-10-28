5a4d5bfa25c5144f680c1629cf7a1ec299c32c24
Georgy Ustinov <inbox@itustinov.ru>
2024-10-28 14:30:16 +0300

    More logs for Nuitka.

--- src/catframes/catmanager_sample/main.py
+++ src/catframes/catmanager_sample/main.py
@@ -14,9 +14,21 @@ def main():
         logger = logging.getLogger('catmanager')
         logger.info('Logging is on.')
 
+        logger.info(f'sys.argv[0]: {sys.argv[0]}')
         logger.info(f'Executable: {sys.executable}')
-        logger.info(f'File: {__file__}')
+        logger.info(f'File: {__file__}\n')
+
+        compiled: bool = '__compiled__' in globals()
+        logger.info(f"'__compiled__' in globals(): {compiled}\n")
+        if compiled:
+            logger.info(f"__compiled__: {globals()['__compiled__']}\n")
+
+        logger.info(f'platform.system(): {platform.system()}')
+        logger.info(f'sys.platform: {sys.platform}\n')
+
         logger.info(f'Console: {has_console()}')
+        logger.info(f'Standard input type: {type(sys.stdin)}')
+        logger.info(f'Standard output type: {type(sys.stdout)}\n')
 
         root = LocalWM.open(RootWindow, 'root')  # открываем главное окно
         root.mainloop()
