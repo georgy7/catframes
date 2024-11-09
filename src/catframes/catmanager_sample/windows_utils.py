329a02884c9b7637ede57275b48decc4097b8518
metacrist <BogdanSir1807@gmail.com>
2024-11-09 20:12:27 +0300

    White overlay on white canvas color bug has been fixed

--- src/catframes/catmanager_sample/windows_utils.py
+++ src/catframes/catmanager_sample/windows_utils.py
@@ -918,7 +918,7 @@ class ImageCanvas(Canvas):
         # если пиксель за пределами картинки, оценивается фон холста
         except Exception:
             r, g, b = self.winfo_rgb(self.color)
-            r, g, b = r / 255, g / 255, b / 255
+            r, g, b = r / 256, g / 256, b / 256
 
         return is_dark_color(r, g, b)
 
