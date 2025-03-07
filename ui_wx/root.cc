633e2ffb785f1d6376e797bf4f2117b33a59483a
Georgy Ustinov <inbox@itustinov.ru>
2025-03-07 17:11:46 +0300

    wx: Entry point

--- ui_wx/root.cc
+++ ui_wx/root.cc
@@ -15,9 +15,27 @@ struct RootFrame::impl {
 
 RootFrame::RootFrame()
     : wxFrame(NULL, wxID_ANY, "Catframes", wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE),
-      p_impl_{std::make_unique<impl>()} {}
+      p_impl_{std::make_unique<impl>()} {
+  wxImage::AddHandler(new wxPNGHandler);
+  wxImage::AddHandler(new wxJPEGHandler);
+}
 
 RootFrame::~RootFrame() {}
+
+// ...............................
+
+class EntryPoint : public wxApp {
+ public:
+  virtual bool OnInit();
+};
+
+IMPLEMENT_APP(EntryPoint);
+
+bool EntryPoint::OnInit() {
+  SetTopWindow(new RootFrame());
+  GetTopWindow()->Show();
+  return true;
+}
 
 }  // namespace wx
 }  // namespace ui
